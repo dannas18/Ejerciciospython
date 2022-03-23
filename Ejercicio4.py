e8024965456f8b6a560b522034fd2fa6ad9680a6
dannas18 <dsfranco61@misena.edu.co>
2022-03-23 08:51:23 -0500

    Ejercicio Python
    Ejercicios Python

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,5 +1,5 @@
 capital=10000
 interes=0.035
 años=7
-valor=capital+(0.035/capital*interes)
-print("el volor futuro es",round(valor))
+valor=capital*(1+interes)**años
+print("el volor futuro es",round(valor,1))
---
+++ Ejercicio5.py
@@ -0,0 +1,6 @@
+x1=3
+x2=-7
+y1=5
+y2=4 
+resultado=((x1-x2)**2+(y1-y2)**2)**(1/2)
+print("la distancia euclideanea es :",(resultado))
