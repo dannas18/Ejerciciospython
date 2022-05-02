345d13fa39faaffaf3ccf1a499f290d9fa8a9c66
dannas18 <dsfranco61@misena.edu.co>
2022-05-02 11:50:19 -0500

    clase02

---
+++ clase.py
@@ -0,0 +1,16 @@
+import math
+#from pickle import APPEND
+#a=[1,2,3,4,5,6]
+#b=[]
+
+#for i in a:
+    #b.append= math.sqrt(i) 
+    #print(b)
+#-----------------
+#n=int(input("Digite un numero entero:"))
+#a= []
+#for i in range(1,n):
+    #a.append(n**3)
+#print(a)
+
+a=[-4,-3,-2,-1, 0,1,2,3,4,]
